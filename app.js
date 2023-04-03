3edb808d1d092e31ac7a03fc74709d2569815c30
Piyush Mittal <piyushmittal578@gmail.com>
2023-04-03 20:53:09 +0530

    y2

--- app.js
+++ app.js
@@ -18,17 +18,17 @@ app.listen(port, () => {
 
 //deploy
 
-app.get('/api', (req, res) => {
-    const path = `/api/item/${v4()}`;
-    res.setHeader('Content-Type', 'text/html');
-    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
+// app.get('/api', (req, res) => {
+//     const path = `/api/item/${v4()}`;
+//     res.setHeader('Content-Type', 'text/html');
+//     res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+//     res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+// });
 
-app.get('/api/item/:slug', (req, res) => {
-    const { slug } = req.params;
-    res.end(`Item: ${slug}`);
-});
+// app.get('/api/item/:slug', (req, res) => {
+//     const { slug } = req.params;
+//     res.end(`Item: ${slug}`);
+// });
 
 //deploy
 
