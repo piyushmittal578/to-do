2142a7fa0785465c51fb836253d7e90537054afa
Piyush Mittal <piyushmittal578@gmail.com>
2023-04-03 13:24:17 +0530

    Vercel3

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import Item from './Schema/Item.js';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-
+import { v4 } from 'uuid';
 const app = express();
 
 //ejs
@@ -15,6 +15,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(80, () => {
     console.log("Listening on port 80");
 })
+
+//deploy
+
+app.get('/api', (req, res) => {
+    const path = `/api/item/${v4()}`;
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+    res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+app.get('/api/item/:slug', (req, res) => {
+    const { slug } = req.params;
+    res.end(`Item: ${slug}`);
+});
+
+//deploy
 
 async function connectDB() {
     const uri = "mongodb+srv://admin578:test578@cluster0.ysqk1jk.mongodb.net/todo?retryWrites=true&w=majority";
@@ -82,3 +98,5 @@ app.post("/", (req, res) => {
         res.redirect("/");
     }
 })
+
+export default app
